860c0ffcf656dc53a23830482a8b1e85c7bf3c99
Shangin Nikolai <shanginn@gmail.com>
2021-05-06 23:12:37 +0400

    v2

--- src/main.rs
+++ src/main.rs
@@ -20,33 +20,44 @@ fn string_to_base64(string: String) -> String {
     ];
 
     let bytes = string.as_bytes();
+    let bytes_count = bytes.len();
+    let full_chunks_count = bytes_count / 3 * 3;
+    let left_bytes_count = bytes_count - full_chunks_count;
 
-    let mut prev: u8 = 0b0000_0000;
-    let mut index;
-    let mut offset;
-    let mut counter = 0;
     let mut result = String::new();
 
-    for (i, byte) in bytes.iter().enumerate() {
-        counter = i % 3;
-        offset = (counter + 1) * 2;
-        index = ((byte >> offset - 2) | prev) >> 2;
-        prev = byte << (8 - offset);
+    for chunk in bytes[..full_chunks_count].chunks(3) {
+        let index1 = chunk[0] >> 2;
+        let index2 = (chunk[0] << 6 | chunk[1] >> 2) >> 2;
+        let index3 = (chunk[1] << 4 | chunk[2] >> 4) >> 2;
+        let index4 = chunk[2] & 0b0011_1111;
 
-        result.push(alphabet[index as usize]);
+        result.push(alphabet[index1 as usize]);
+        result.push(alphabet[index2 as usize]);
+        result.push(alphabet[index3 as usize]);
+        result.push(alphabet[index4 as usize]);
+    }
 
-        if counter == 2 {
-            result.push(alphabet[(prev >> 2) as usize]);
+    if left_bytes_count > 0 {
+        let left_bytes = &bytes[full_chunks_count..];
+        let index1 = left_bytes[0] >> 2;
+        result.push(alphabet[index1 as usize]);
 
-            prev = 0b0000_0000;
+        if left_bytes_count == 1 {
+            let index2 = (left_bytes[0] << 6) >> 2;
+
+            result.push(alphabet[index2 as usize]);
+            result.push('=');
         }
-    }
 
-    if prev != 0b0000_0000 {
-        result.push(alphabet[(prev >> 2) as usize]);
-    }
+        if left_bytes_count == 2 {
+            let index2 = (left_bytes[0] << 6 | left_bytes[1] >> 2) >> 2;
+            let index3 = (left_bytes[1] << 4) >> 2;
 
-    for _i in 0..(2 - counter) {
+            result.push(alphabet[index2 as usize]);
+            result.push(alphabet[index3 as usize]);
+        }
+
         result.push('=');
     }
 
